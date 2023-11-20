fd45504996e7f42783befcfd5f2589113e442363
nagoorsky <nagoorsky@gmail.com>
2023-11-20 23:46:57 +0100

    feat: products list view wip

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,18 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  DxButtonModule,
+  DxListModule,
+  DxSelectBoxModule,
+  DxTileViewModule,
+} from 'devextreme-angular';
 import { Observable } from 'rxjs';
 
 import { ProductService } from '../../services/products.service';
+import { dxItem } from 'devextreme/ui/widget/ui.widget';
 
 @Component({
   standalone: true,
   selector: 'product-list',
   templateUrl: './product-list.component.html',
-  imports: [CommonModule],
+  styleUrls: ['./product-list.component.scss'],
+  imports: [
+    CommonModule,
+    DxSelectBoxModule,
+    DxListModule,
+    DxTileViewModule,
+    DxButtonModule,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductListComponent {
   private readonly productService = inject(ProductService);
 
-  list$: Observable<any[]> = this.productService.getProductsList();
+  viewOptions = [
+    { id: 'list', text: 'Lista' },
+    { id: 'tiles', text: 'Kafelki' },
+  ];
+  selectedView: string = 'tiles';
+
+  productsList$: Observable<any[]> = this.productService.getProductsList();
+
+  navigateToProduct(id: number) {
+    console.log(id);
+  }
 }
